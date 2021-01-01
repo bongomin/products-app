d0733d899d1fd01c844db3cb92ba92913c8ca86b
bongomin <candanobongomindaniel@gmail.com>
2021-01-01 12:26:57 +0300

    initial commit of the project code

---
+++ src/Pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <div class="container">
+      home / Dashboard
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
