04f55c5e8470141a1a9ffe45ed455392ce136cef
bongomin <candanobongomindaniel@gmail.com>
2021-01-01 13:17:32 +0300

    added side bar menu and intergrated mdi/font

--- src/Pages/Home.vue
+++ src/Pages/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
     <NavbarComponent />
-    <div class="container"></div>
+    <div class="container">
+      <div class="row">
+        <SideMenuComponents />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import NavbarComponent from "../components/Header/Navbar.vue";
+import SideMenuComponents from "../components/SideMenu/SideMenu.vue";
 
 export default {
   name: "Home",
   components: {
-    NavbarComponent
+    NavbarComponent,
+    SideMenuComponents
   }
 };
 </script>
